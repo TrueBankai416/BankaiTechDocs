43bd508bb3b058c87ed22f87115408876bf0fdfb
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2024-04-18 19:22:02 -0400

    Update index.js

--- src/components/GiscusComponents/index.js
+++ src/components/GiscusComponents/index.js
@@ -7,21 +7,19 @@ export default function GiscusComponent() {
 
   return (
     <Giscus    
-      repo="ghUsername/repoName"
-      repoId="someHash"
-      category="Announcements"
-      categoryId="someHash"
-      mapping="title"
-      term="specific-term" //If you didn't select "Discussion title contains a specific term", omit.
-      strict="0"
-      reactionsEnabled="1"
-      emitMetadata="1"
-      inputPosition="top"
-      theme={colorMode}
-      lang="en"
-      loading="lazy"
-      crossorigin="anonymous"
-      async
+        data-repo="TrueBankai416/BankaiTechDocs"
+        data-repo-id="R_kgDOLsjlxA"
+        data-category="General"
+        data-category-id="DIC_kwDOLsjlxM4CeyWI"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
     />
   );
 }
