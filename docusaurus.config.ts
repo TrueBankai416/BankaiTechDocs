ef6dea1f329694a8cb6b8b446bd27ab688cee2af
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2024-04-18 21:23:53 -0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,12 +73,12 @@ const config: Config = {
     metadata: [
       {name: 'keywords', content: 'Docs, Nextcloud, Tutorial, Documentation'},
     ],
-    forbiddenGiscusDocPaths: [
-    ''
-    ],
-    forbiddenGiscusBlogPaths: [
-    ''
-    ],
+ //   forbiddenGiscusDocPaths: [
+ //   ''
+ //   ],
+ //   forbiddenGiscusBlogPaths: [
+ //   ''
+ //   ],
     sidebar: {
       autoCollapseCategories: true
     },
