f56fec3353a690f4a75f4985c297d4f2a9810db1
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2024-12-07 04:39:02 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,21 +62,25 @@ const config: Config = {
       [
         'posthog-docusaurus',
         {
+          id: 'posthog'
           apiKey: 'POSTHOG_API_KEY',
           appUrl: 'https://us.i.posthog.com', // optional, defaults to "https://us.i.posthog.com"
           enableInDevelopment: false, // optional
         },
       ],
-//        'docusaurus-plugin-dotenv',
-//        {
-//          path: "./.env", // The path to your environment variables.
-//          safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
-//          systemvars: false, // Set to true if you would rather load all system variables as well (useful for CI purposes)
-//          silent: false, //  If true, all warnings will be suppressed
-//          expand: false, // Allows your variables to be "expanded" for reusability within your .env file
-//          defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
-//         ignoreStub: true
-//        },
+      [
+        'docusaurus-plugin-dotenv',
+        {
+          id: 'dotenv'
+          path: "./.env", // The path to your environment variables.
+          safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
+          systemvars: false, // Set to true if you would rather load all system variables as well (useful for CI purposes)
+          silent: false, //  If true, all warnings will be suppressed
+          expand: false, // Allows your variables to be "expanded" for reusability within your .env file
+          defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
+         ignoreStub: true
+        },
+      ],
     ],
 
   themeConfig: {
