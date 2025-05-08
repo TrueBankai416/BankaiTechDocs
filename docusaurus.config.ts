3d4f2d5dde61925b0175d35a8fe2a39feb230641
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-05-08 01:07:23 -0400

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://docs.bankai-tech.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
+  // Set the /<baseUrl>/ pathname under which your site is servedd
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
