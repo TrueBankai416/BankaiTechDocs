74cb4632d671fb7469f21e548953b10480f98e7b
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-07-13 19:53:05 +0000

    Remove incorrectly added ReactLiveScope component
    The ReactLiveScope component is included by default with @docusaurus/theme-live-codeblock and doesn't need to be manually created. The issue with live code blocks showing "results loading" is likely caused by a different problem that needs further investigation.
    
    Relates to # 257
    
    Mentat precommit script passed. Log: https://mentat.ai/gh/TrueBankai416/BankaiTechDocs/log/fe4fc77d-e692-459f-92de-b239fe1a49c8
    
    Co-authored-by: TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>

--- src/theme/ReactLiveScope/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// Add any additional components you want to make available in live code blocks
-const ReactLiveScope = {
-  React,
-  ...React,
-};
-
-export default ReactLiveScope;
