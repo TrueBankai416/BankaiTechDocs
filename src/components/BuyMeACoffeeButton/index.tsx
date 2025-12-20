57c2c58d967a6b01e04104fee725868fdedc711c
TrueBankai416 <carrigan@lunekes.com>
2025-12-19 22:32:22 -0500

    Update BuyMeACoffeeButton image source color for better visibility

--- src/components/BuyMeACoffeeButton/index.tsx
+++ src/components/BuyMeACoffeeButton/index.tsx
@@ -7,7 +7,7 @@ interface BuyMeACoffeeButtonProps {
 const BuyMeACoffeeButton: React.FC<BuyMeACoffeeButtonProps> = ({ className }) => {
   return (
     <a href="https://www.buymeacoffee.com/BankaiTech" className={className}>
-      <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a beer&emoji=🍻&slug=BankaiTech&button_colour=ff0000&font_colour=ffffff&font_family=Cookie&outline_colour=ffffff&coffee_colour=FFDD00" />
+      <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a beer&emoji=🍻&slug=BankaiTech&button_colour=BD5FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" />
     </a>
   );
 };
